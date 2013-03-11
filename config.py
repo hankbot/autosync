dbdb367cc5611d5e87c2636522ffd36741d7b561
M. H. Kiedrowski <matt@gooddaysam.com>
2013-03-11 17:40:11 -0500

    Cleaned up rsync options

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ LOG_LEVEL = logging.WARNING
 FSEVENT_LATENCY = 4
 FSEVENT_SINCE   = -1
 
-RSYNC_OPTIONS = ['-v', '--delete', '-rltu', '--exclude=".svn"', '--exclude="data/*"']
+RSYNC_OPTIONS = ['-v', '--delete', '-rltu', '--exclude=.svn/', '--exclude=data/']
